80fbca92dbb10b36e4af1529b6b39145fdb83479
Charles D'Angelo <c.g.dangelo@gmail.com>
2013-06-06 20:25:26 -0400

    Fix check_value for directory_entries_offset
    Fixes #1

--- lib/demoparse/header.rb
+++ lib/demoparse/header.rb
@@ -3,18 +3,19 @@ require 'demoparse/directory_entry'
 module Demoparse
   class Header < BinData::Record
     HEADER_LENGTH = 544
-
     endian :little
+    count_bytes_remaining :bytes_remaining
+
     string :gametype, :read_length => 8, :trim_padding => true
     uint32 :demo_protocol, :check_value => 5
     uint32 :network_protocol
     string :mapname, :read_length => 260, :trim_padding => true
     string :gamefolder, :read_length => 260, :trim_padding => true
     uint32 :mapchecksum
-    uint32 :directory_entries_offset, :check_value => lambda { num_bytes - 4 - (Demoparse::DirectoryEntry::DIRECTORY_ENTRY_LENGTH * 2) }
-    int32  :directory_entries_total, :adjust_offset => :directory_entries_offset, :check_value => 2, :value => lambda { directory_entries.length }
 
-    array :directory_entries, :initial_length => 2 do
+    uint32le :directory_entries_offset, :check_value => lambda { bytes_remaining - 4 - (2 * DirectoryEntry::DIRECTORY_ENTRY_LENGTH) }
+    int32le  :directory_entries_total, :adjust_offset => :directory_entries_offset, :check_value => 2, :value => lambda { directory_entries.length }
+    array  :directory_entries, :initial_length => :directory_entries_total do
       directory_entry :directory_entry
     end
   end
